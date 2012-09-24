5f8818ece8e931ad466a1c597476a11edd3981a7
Buganini Q <buganini@gmail.com>
2012-09-24 14:50:28 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup,Extension
 
 setup(name='bsdconv',
-      version='8.0',
+      version='9.0',
       description='bsdconv library',
       author='Buganini',
       author_email='buganini@gmail.com',
       url='http://github.com/buganini/bsdconv',
       ext_modules=[Extension('bsdconv', ['bsdconv.c'],
-              include_dirs=['/usr/local/include'],
+              include_dirs=['/usr/local/include','c:/bsdconv/include'],
               libraries=['bsdconv'],
-              library_dirs=['/usr/local/lib'])],
+              library_dirs=['/usr/local/lib','c:/bsdconv'])],
      )
 
